1da57a832c2dbe9bc78a54d24c24f008b94f2852
annie.ro98 <anniero@berkeley.edu>
2020-03-22 02:05:22 -0700

    Cleanup code for Airtable API calls

--- airtable.js
+++ airtable.js
@@ -1,5 +1,4 @@
 /* eslint no-restricted-imports: 0 */
-
 /*
   THIS IS A GENERATED FILE
   Changes might be overwritten in the future, edit with caution!
@@ -9,13 +8,12 @@
 
   If you're adding a new function: make sure you add a corresponding test (at least 1) for it in airtable.spec.js
 */
+import Airtable from "airtable";
+import { Columns } from "./schema";
 
-import Airtable from 'airtable';
-import { Columns } from './schema';
-
-const BASE_ID = 'REPLACE_BASE_ID';
-const VIEW = 'REPLACE_VIEW';
-const ENDPOINT_URL = 'https://api.airtable.com';
+const BASE_ID = "REPLACE_BASE_ID";
+const VIEW = "REPLACE_VIEW";
+const ENDPOINT_URL = "https://api.airtable.com";
 
 const apiKey = process.env.REACT_APP_AIRTABLE_API_KEY;
 
@@ -58,7 +56,7 @@ const fromAirtableFormat = (record, table) => {
     let value = record[origColumn];
 
     // Unwrap array if it's a single foreign key relationship
-    if (jsFormattedName.type === 'foreignKey-one') {
+    if (jsFormattedName.type === "foreignKey-one") {
       [value] = value; // Array Destructuring
     }
 
@@ -87,7 +85,7 @@ const toAirtableFormat = (record, table) => {
     }
 
     let value = record[jsFormattedColumnName];
-    if (origColumn.type === 'foreignKey-one') {
+    if (origColumn.type === "foreignKey-one") {
       value = [value]; // rewrap array if it's a single foreign key relationship
     }
 
@@ -98,127 +96,13 @@ const toAirtableFormat = (record, table) => {
 // ******** CRUD ******** //
 // Given a table and a record object, create a record on Airtable.
 function createRecord(table, record) {
-  return new Promise(function(resolve, reject) {
-    const transformedRecord = toAirtableFormat(record, table);
-    base(table).create([{ fields: transformedRecord }], function(err, records) {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      const expectedLen = 1;
-      if (records.length !== expectedLen) {
-        reject(
-          new Error(
-            `${records.length} records returned from creating 1 record. Expected: ${expectedLen}`
-          )
-        );
-        return;
-      }
-
-      resolve(records[0].getId());
-    });
-  });
-}
-
-// TODO pagination?
-// TODO: current implementation only fetches the first page
-function getAllRecords(table) {
-  return new Promise(function(resolve, reject) {
+  const transformedRecord = toAirtableFormat(record, table);
+  return new Promise(function process(resolve, reject) {
     base(table)
-      .select({
-        view: VIEW
-      })
-      .eachPage(
-        function page(records, fetchNextPage) {
-          if (records === null || records.length < 1) {
-            const msg = `No record was retrieved using this ${table}.`;
-            reject(msg);
-            return;
-          }
-
-          resolve(
-            records.map(record => fromAirtableFormat(record.fields, table))
-          );
-
-          // To fetch the next page of records, call `fetchNextPage`.
-          // If there are more records, `page` will get called again.
-          // If there are no more records, `done` will get called.
-          fetchNextPage();
-        },
-        function done(err) {
-          if (err) {
-            reject(err);
-          }
-        }
-      );
-  });
-}
-
-// Given a table and record ID, return the associated record object using a Promise.
-function getRecordById(table, id) {
-  return new Promise(function(resolve, reject) {
-    base(table).find(id, function(err, record) {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      resolve(fromAirtableFormat(record.fields, table));
-    });
-  });
-}
-
-// TODO: current implementation only returns the first page
-/*
-  Given the desired table, field type (column), and field ('nick wong' or 'aivant@pppower.io'),
-  return the associated record object.
-*/
-function getRecordsByAttribute(table, fieldType, field) {
-  return new Promise(function(resolve, reject) {
-    base(table)
-      .select({
-        view: VIEW,
-        filterByFormula: `{${fieldType}}='${field}'`
-      })
-      .firstPage((err, records) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        if (!records || records.length < 1) {
-          console.log(`No record was retrieved using this ${fieldType}.`);
-          resolve([]);
-          // No need for this to throw an error, sometimes there's just no values
-          // reject(new Error(`No record was retrieved using this ${fieldType}.`));
-          return;
-        }
-
-        resolve(
-          records.map(record => fromAirtableFormat(record.fields, table))
-        );
-      });
-  });
-}
-
-// Given a table and a record object, update a record on Airtable.
-function updateRecord(table, id, updatedRecord) {
-  return new Promise(function(resolve, reject) {
-    const transformedRecord = toAirtableFormat(updatedRecord, table);
-    base(table).update(
-      [
-        {
-          id,
-          fields: transformedRecord
-        }
-      ],
-      function(err, records) {
-        if (err) {
-          reject(err);
-          return;
-        }
-
+      .create([{ fields: transformedRecord }])
+      .then(records => {
         const expectedLen = 1;
+        // TODO will this error ever be triggered? It seems like AirTable should catch and throw this error
         if (records.length !== expectedLen) {
           reject(
             new Error(
@@ -228,32 +112,110 @@ function updateRecord(table, id, updatedRecord) {
           return;
         }
 
-        resolve(records[0].id); // TODO
-      }
-    );
+        resolve(records[0].getId());
+      })
+      .catch(err => reject(err));
   });
 }
 
-function deleteRecord(table, id) {
-  return new Promise(function(resolve, reject) {
-    base(table).destroy([id], function(err, deletedRecords) {
-      if (err) {
-        reject(err);
-        return;
-      }
-      const expectedLen = 1;
-      if (deletedRecords.length !== expectedLen) {
-        reject(
-          new Error(
-            `${deletedRecords.length} records returned from deleting ${expectedLen} record(s). Expected: ${expectedLen}`
-          )
-        );
-        return;
+function getAllRecords(table, filterByFormula = "", sort = []) {
+  return base(table)
+    .select({
+      view: VIEW,
+      filterByFormula,
+      sort
+    })
+    .all()
+    .then(records => {
+      if (records === null || records.length < 1) {
+        const msg = `No record was retrieved using this ${table}.`;
+        throw new Error(msg);
       }
 
-      resolve(deletedRecords[0].fields);
+      return records.map(record => fromAirtableFormat(record.fields, table));
+    })
+    .catch(err => {
+      throw err;
     });
-  });
+}
+
+// Given a table and record ID, return the associated record object using a Promise.
+function getRecordById(table, id) {
+  return base(table)
+    .find(id)
+    .then(record => {
+      return fromAirtableFormat(record.fields, table);
+    })
+    .catch(err => {
+      throw err;
+    });
+}
+
+/*
+  Given the desired table, field type (column), and field ('nick wong' or 'aivant@pppower.io'),
+  return the associated record object.
+*/
+function getRecordsByAttribute(table, fieldType, field, sort = []) {
+  return base(table)
+    .select({
+      view: VIEW,
+      filterByFormula: `{${fieldType}}='${field}'`,
+      sort
+    })
+    .all()
+    .then(records => {
+      if (!records || records.length < 1) {
+        console.log(`No record was retrieved using this ${fieldType}.`);
+        return [];
+        // No need for this to throw an error, sometimes there're just no values
+      }
+
+      return records.map(record => fromAirtableFormat(record.fields, table));
+    })
+    .catch(err => {
+      throw err;
+    });
+}
+
+// Given a table and a record object, update a record on Airtable.
+function updateRecord(table, id, updatedRecord) {
+  const transformedRecord = toAirtableFormat(updatedRecord, table);
+  return base(table)
+    .update([
+      {
+        id,
+        fields: transformedRecord
+      }
+    ])
+    .then(records => {
+      const expectedLen = 1;
+      if (records.length !== expectedLen) {
+        throw new Error(
+          `${records.length} records returned from updating 1 record. Expected: ${expectedLen}`
+        );
+      }
+      return records[0].id;
+    })
+    .catch(err => {
+      throw err;
+    });
+}
+
+function deleteRecord(table, id) {
+  return base(table)
+    .destroy([id])
+    .then(records => {
+      const expectedLen = 1;
+      if (records.length !== expectedLen) {
+        throw new Error(
+          `${records.length} records returned from deleting ${expectedLen} record(s). Expected: ${expectedLen}`
+        );
+      }
+      return records[0].fields;
+    })
+    .catch(err => {
+      throw err;
+    });
 }
 
 export {
