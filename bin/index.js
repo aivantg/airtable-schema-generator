168b95cb8248c0b611ddf4299a664add03b586dc
aivantg <aivant.developer@gmail.com>
2020-03-22 19:53:09 -0500

    switched to dotenv

--- bin/index.js
+++ bin/index.js
@@ -9,10 +9,8 @@ const {
 } = require('../lib/generators');
 
 // Load regular config vars
-require('dotenv-safe').config({
-  path: '.env',
-  example: path.resolve(__dirname, '../.env.example'),
-  allowEmptyValues: true
+require('dotenv').config({
+  path: '.env'
 });
 
 const script = `copy(_.mapValues(application.tablesById, table => _.set(_.omit(table, ['sampleRows']),'columns',_.map(table.columns, item =>_.set(item, 'foreignTable', _.get(item, 'foreignTable.id'))))));`;
